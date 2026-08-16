792e6a95fa3c3d415a64b25ad755f2ebfd754192
MOoshalll <Mbkturner@gmail.com>
2026-08-16 14:44:59 -0600

    you goobers better win this

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+//imports
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@TeleOp(name = "Main Teleop")
+public class Teleop extends LinearOpMode{
+
+
+    @Override
+    public void runOpMode(){
+        //create subsystem objects here
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            //put loop code here
+        }
+
+    }
+
+
+}
