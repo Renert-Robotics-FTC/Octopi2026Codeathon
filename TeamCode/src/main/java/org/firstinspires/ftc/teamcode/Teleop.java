ccf9819599b5bd2fbedac04f8a7e3143d259f191
Tracy Ma <nicholas12ma@gmail.com>
2026-09-02 01:01:07 -0600

    added odometry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -7,15 +7,25 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name = "Main Teleop")
 public class Teleop extends LinearOpMode {
 
+    Odometry odometry = new Odometry();
 
     @Override
     public void runOpMode() {
 
+        odometry.initializeOdometry(hardwareMap);
 
         waitForStart();
 
         while (opModeIsActive()) {
-            //write looping code
+            odometry.updateOdometry();
+
+            //report data to telemetry
+            telemetry.addData("x Position", odometry.getxPos());
+            telemetry.addData("y Position", odometry.getyPos());
+            telemetry.addData("Heading", odometry.getHeading());
+
+            //communicate to driver through telemetry
+            telemetry.update();
 
         }
     }
